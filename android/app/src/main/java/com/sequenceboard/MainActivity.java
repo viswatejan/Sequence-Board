8a6fefd5b43da299248d88d572db15a2e44a1eb7
Viswa Teja Nariboina <viswatejan@gmail.com>
2018-04-19 18:21:00 -0500

    always on screen, immersive mode

--- android/app/src/main/java/com/sequenceboard/MainActivity.java
+++ android/app/src/main/java/com/sequenceboard/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sequenceboard;
 
+import android.view.View;
+import android.view.WindowManager;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +13,30 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "sequenceboard";
+    }
+
+    @Override
+    protected void onStart(){
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        hideSystemUI();
+        super.onStart();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI(){
+        getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
